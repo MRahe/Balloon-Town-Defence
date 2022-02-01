61cb48f6f2f1f434f4ec2e32bb26674522f78602
MRahe <42557390+MRahe@users.noreply.github.com>
2022-02-01 17:01:35 -0600

    Added VR text and potential fix for pop

--- Roll-a-ball/Assets/Scripts/Pop.cs
+++ Roll-a-ball/Assets/Scripts/Pop.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Pop : MonoBehaviour
 {
+    public int balloonsPopped;
+    public TextMeshProUGUI balloonCountText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        balloonCountText = GameObject.FindGameObjectWithTag("count").GetComponent<TextMeshProUGUI>();
+        balloonsPopped = 0;
+        setCountText(); 
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Bang()
     {
-        
+        print("balloon popped.");
+        balloonsPopped++;
+        setCountText(); 
+    }
+
+
+    void setCountText()
+    {
+        balloonCountText.text = "Balloons Popped " + balloonsPopped.ToString();
     }
 }
